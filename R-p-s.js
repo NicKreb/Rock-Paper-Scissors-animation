3c44b762b42ebeb9537c27c362776c17c84de1c7
NicKreb <113550184+NicKreb@users.noreply.github.com>
2023-03-01 18:32:53 -0600

    Create R-p-s.js

---
+++ R-p-s.js
@@ -0,0 +1,14 @@
+/* for (let i=0, i > 0, i++;) {
+if (i = 1, && i = 4, && i = 7;){
+_____ = < >}
+else if (i = 2, && i = 5, && i = 8;) {
+____ = < >
+}
+else if (i = 3, && i = 6, && i = 9;) {
+____ = < >
+}
+else {
+results = ;
+return results
+}
+}; */
