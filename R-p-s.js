9c1dec52b405289ca7785534a16717af4e9be416
NicKreb <113550184+NicKreb@users.noreply.github.com>
2023-03-12 14:22:38 -0500

    Update r-p-s-Shoot 03.12.23
    cleared out everything previously on the js document. Instead created the array with the string of each div for the symbols.
    then created a function called 'shoot()' to run with the html onClick function.
    And lastly: started on a function for the animation, which will eventually be the primary function of the html button, of all the hand symbols that will then call on the shoot() function.

--- R-p-s.js
+++ R-p-s.js
@@ -1,18 +1,18 @@
-/* for (let i=0, i > 0, i++;) {
-if (i = 1, && i = 4, && i = 7;){
-_____ = < >}
-else if (i = 2, && i = 5, && i = 8;) {
-____ = < >
-}
-else if (i = 3, && i = 6, && i = 9;) {
-____ = < >
-}
-else {
-results = ;
-return results
-}
-}; */
+var rpsArr = [
+      "<div class='rock'><div class='rockhand rockPointerFinger'></div><div class='rockhand rockMiddleFinger'></div><div class='rockhand rockRingFinger'></div><div class='rockhand rockPinkyFinger'></div><div class='rockhand rockThumb'></div><div class='rockhand rockPalm'></div></div>",
+      '<div class="scissors scissorsPointerFinger"></div><div class="scissors scissorsMiddleFinger"></div><div class="scissors scissorsRingFinger"></div><div class="scissors scissorsPinkyFinger"></div><div class="scissors scissorsThumb"></div><div class="scissors scissorsPalm"></div>', 
+      '<div class="paper paperPointerFinger"> <div class="paperPointBackground"></div></div><div class="paper paperMiddleFinger"><div class="paperMidBackground"></div></div><div class="paper paperRingFinger"><div class="paperRingBackground"></div></div><div class="paper paperPinkyFinger"><div class="paperPinkyBackground"></div></div> <div class="paper paperThumb"><div class="paperThumbBackground"></div></div><div class="paper paperPalm"><div class="paperPalmBackground"></div></div>'
+    ];
 
-document.getElementById("Shoot") {
+/* V Function that inputs one of the hands in to the box as a final result V */
+
+function shoot() {
+  document.getElementById("box").innerHTML = 
+    rpsArr[Math.floor(Math.random() * rpsArr.length)];
+};
+
+/* V Function that will cycle through the above images and then finish with the shoot function result V */
+
+function rpsAnimation() {
   
-}
+};
