5bc4e354963a842108fea3f4fcafa2479dedf26a
NicKreb <113550184+NicKreb@users.noreply.github.com>
2023-03-20 18:57:05 -0500

    updated 03.20.23
    edited the function() shoot to have a timer and run off of a separate class and function.
    
    secondary function goes through and changes the display values of the new div elements until it times out and produces one of the hand signs.

--- R-p-s.js
+++ R-p-s.js
@@ -1,18 +1,33 @@
-var rpsArr = [
-      "<div class='rock'><div class='rockhand rockPointerFinger'></div><div class='rockhand rockMiddleFinger'></div><div class='rockhand rockRingFinger'></div><div class='rockhand rockPinkyFinger'></div><div class='rockhand rockThumb'></div><div class='rockhand rockPalm'></div></div>",
-      '<div class="scissors scissorsPointerFinger"></div><div class="scissors scissorsMiddleFinger"></div><div class="scissors scissorsRingFinger"></div><div class="scissors scissorsPinkyFinger"></div><div class="scissors scissorsThumb"></div><div class="scissors scissorsPalm"></div>', 
-      '<div class="paper paperPointerFinger"> <div class="paperPointBackground"></div></div><div class="paper paperMiddleFinger"><div class="paperMidBackground"></div></div><div class="paper paperRingFinger"><div class="paperRingBackground"></div></div><div class="paper paperPinkyFinger"><div class="paperPinkyBackground"></div></div> <div class="paper paperThumb"><div class="paperThumbBackground"></div></div><div class="paper paperPalm"><div class="paperPalmBackground"></div></div>'
-    ];
 
-/* V Function that inputs one of the hands in to the box as a final result V */
+var visibleIdx = 0;
+
+var options = document.getElementsByClassName("option");
+
+var startTime = 0;
+
 
 function shoot() {
-  document.getElementById("box").innerHTML = 
-    rpsArr[Math.floor(Math.random() * rpsArr.length)];
+    startTime = Date.now();
+    
+  loopUntilTime(startTime + 5000);
+  document.getElementById("filler").classList.toggle("tickdown");
 };
 
-/* V Function that will cycle through the above images and then finish with the shoot function result V */
-
-function rpsAnimation() {
+function loopUntilTime(endTime){
+    var newIdx;
+  do {
+    newIdx = Math.floor(Math.random() * 3);
+  }
+  while (newIdx === visibleIdx);
+  options[visibleIdx].style.display = "none";
+  visibleIdx = newIdx;
+  options[visibleIdx].style.display="";
   
-};
+  if (Date.now() < endTime)
+  {
+    setTimeout(() => loopUntilTime(endTime), 300);
+  }
+  else{
+    document.getElementById("filler").classList.toggle("tickdown");
+  }
+}
